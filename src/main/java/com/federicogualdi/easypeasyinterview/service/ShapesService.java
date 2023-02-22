d7d165bddf3071cd741f8b9236f9aeb13d85731a
Federico Gualdi <federicogualdi98@gmail.com>
2023-02-22 15:13:56 +0100

    test: refactoring code to improve test coverage

--- src/main/java/com/federicogualdi/easypeasyinterview/service/ShapesService.java
+++ src/main/java/com/federicogualdi/easypeasyinterview/service/ShapesService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.List;
 
 @ApplicationScoped
 public class ShapesService {
@@ -13,10 +14,14 @@ public class ShapesService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Inject
-    DataService dataService;
+    private DataService dataService;
 
     public RectangleDto computeRectangleData() {
-        logger.debug("xPoints {} | yPoints {}", dataService.getRectangleXPoints(), dataService.getRectangleYPoints());
+        return computeRectangleData(dataService.getRectangleXPoints(), dataService.getRectangleYPoints());
+    }
+
+    protected RectangleDto computeRectangleData(List<Integer> xPoints, List<Integer> yPoints) {
+        logger.debug("xPoints {} | yPoints {}", xPoints, yPoints);
         // WRITE HERE YOUR CODE
         var dto = new RectangleDto();
         return dto;
--- src/test/java/com/federicogualdi/easypeasyinterview/service/ShapeServiceTest.java
+++ src/test/java/com/federicogualdi/easypeasyinterview/service/ShapeServiceTest.java
@@ -7,17 +7,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @QuarkusTest
 public class ShapeServiceTest {
     @Inject ShapesService shapesService;
 
     @Test
-    public void testRectangleComputation(){
-        var rectangle = shapesService.computeRectangleData();
+    public void testRectangleComputation() {
+        var rectangle = shapesService.computeRectangleData(List.of(3, 3, 4, 4), List.of(2, 5, 5, 2));
         Assertions.assertAll("It should ensure the correctness of the calculation of the rectangle data",
-                ()->Assertions.assertEquals(rectangle.perimeter, 8),
-                ()->Assertions.assertEquals(rectangle.area, 3)
+                () -> Assertions.assertEquals(rectangle.perimeter, 8),
+                () -> Assertions.assertEquals(rectangle.area, 3)
+        );
+    }
+
+    @Test
+    public void testRotatedRectangleComputation() {
+        var rectangle = shapesService.computeRectangleData(List.of(1, 4, 3, 0), List.of(0, 3, 4, 1));
+        Assertions.assertAll("It should ensure the correctness of the calculation of the rectangle data",
+                () -> Assertions.assertEquals(Math.round(rectangle.perimeter), Math.round(11.31370849898476)),
+                () -> Assertions.assertEquals(rectangle.area, 6.0)
         );
     }
 }
